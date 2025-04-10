3c6d028f2f93cf7cfaa4690a13cdff2716c46e80
Andreacolaci99 <colaciandrea99@gmail.com>
2025-04-10 18:54:19 +0200

    feat : with error

--- src/main/java/it/lessons/pizzeria/controller/PizzeriaController.java
+++ src/main/java/it/lessons/pizzeria/controller/PizzeriaController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import it.lessons.pizzeria.model.Pizza;
 import it.lessons.pizzeria.repository.PizzaRepository;
+
 
 @Controller
 @RequestMapping("/")
@@ -29,6 +31,13 @@ public class PizzeriaController {
         model.addAttribute("pizze", listaPizze);
         return "/pizzeria/pizze";
     }
+
+    @GetMapping("/pizzeria/pizze/{id}")
+    public String dettaglioPizza(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("pizza", pizzaRepository.findById(id).get());
+        return "pizzeria/dettaglioPizze";
+    }
+    
 
 }
         
